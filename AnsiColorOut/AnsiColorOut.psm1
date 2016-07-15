d6679253774feb298c2dd505d76925e7df3d61b0
Marko Bozikovic <bozho@kset.org>
2016-07-15 23:29:52 +0100

    Not using PSCX [accelerators] accelerator

--- AnsiColorOut/AnsiColorOut.psm1
+++ AnsiColorOut/AnsiColorOut.psm1
@@ -1,5 +1,6 @@
 # set accelerators
-[accelerators]::Add("AnsiColorOut", [Bozho.PowerShell.AnsiColorOut])
+$accel = [psobject].Assembly.GetType('System.Management.Automation.TypeAccelerators')
+$accel::Add("AnsiColorOut", [Bozho.PowerShell.AnsiColorOut])
 
 # add type extensions and custom formats
 Update-TypeData -AppendPath $PSScriptRoot\types.ps1xml
